ef6411f64d0ca3159065a29fcbcdf35287d8976b
Daniel <contact@daniel-rubin.de>
2023-12-24 12:19:23 +0100

    feat: implemented resend instead on nodemailer

--- app/api/send-mail/route.tsx
+++ app/api/send-mail/route.tsx
@@ -1,5 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
-import SMTPTransport from "nodemailer/lib/smtp-transport";
+import { Resend } from "resend";
+
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function POST(req: NextRequest) {
   require("dotenv").config();
@@ -18,48 +20,66 @@ export async function POST(req: NextRequest) {
   try {
     const body: { name: string; email: string; message: string } =
       await req.json();
-
-    await new Promise((resolve, reject) => {
-      transporter.verify((error: Error | null, success: boolean) => {
-        if (error) {
-          console.log(`Verify-error: ${error}`);
-          reject(error);
-        } else {
-          resolve(success);
-        }
-      });
-    });
-
-    const mailData = {
+    const data = await resend.emails.send({
       from: body.email,
-      to: process.env.SMTP_TO_EMAIL,
+      to: process.env.SMTP_TO_EMAIL!,
       subject: `New Project request from ${body.name}`,
-      text: body.message + " | Sent from: " + body.email,
       html: body.message,
-    };
-
-    await new Promise(async (resolve, reject) => {
-      await transporter.sendMail(
-        mailData,
-        (err: Error | null, info: SMTPTransport.SentMessageInfo) => {
-          if (err) {
-            console.log(`sendMail-error: ${err}`);
-            reject(err);
-          } else {
-            resolve(info);
-          }
-        }
-      );
     });
 
+    return NextResponse.json(data);
+  } catch (error) {
     return NextResponse.json(
-      { message: "Email sent successfully" },
-      { status: 200 }
-    );
-  } catch (err) {
-    return NextResponse.json(
-      { message: `Error while sending email ${err}` },
+      { message: `Error while sending email ${error}` },
       { status: 500 }
     );
   }
+
+  // try {
+  //   const body: { name: string; email: string; message: string } =
+  //     await req.json();
+  //
+  //   await new Promise((resolve, reject) => {
+  //     transporter.verify((error: Error | null, success: boolean) => {
+  //       if (error) {
+  //         console.log(`Verify-error: ${error}`);
+  //         reject(error);
+  //       } else {
+  //         resolve(success);
+  //       }
+  //     });
+  //   });
+  //
+  //   const mailData = {
+  //     from: body.email,
+  //     to: process.env.SMTP_TO_EMAIL,
+  //     subject: `New Project request from ${body.name}`,
+  //     text: body.message + " | Sent from: " + body.email,
+  //     html: body.message,
+  //   };
+  //
+  //   await new Promise(async (resolve, reject) => {
+  //     await transporter.sendMail(
+  //       mailData,
+  //       (err: Error | null, info: SMTPTransport.SentMessageInfo) => {
+  //         if (err) {
+  //           console.log(`sendMail-error: ${err}`);
+  //           reject(err);
+  //         } else {
+  //           resolve(info);
+  //         }
+  //       }
+  //     );
+  //   });
+  //
+  //   return NextResponse.json(
+  //     { message: "Email sent successfully" },
+  //     { status: 200 }
+  //   );
+  // } catch (err) {
+  //   return NextResponse.json(
+  //     { message: `Error while sending email ${err}` },
+  //     { status: 500 }
+  //   );
+  // }
 }
